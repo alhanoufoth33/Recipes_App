adf1245158b92f1f68975995768285667c3b4ed6
alhanoufoth33 <alhanoufoth33@gmail.com>
2019-04-03 16:30:10 +0300

    Updates

---
+++ precache-manifest.46dc1d131790fbda5002c2709d80e601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d9355f1642486fd1208dc65bcb359a",
+    "url": "/Recipes_App/static/media/baguette.23d9355f.jpg"
+  },
+  {
+    "revision": "277104b8da4179361c7f",
+    "url": "/Recipes_App/static/js/runtime~main.824093ee.js"
+  },
+  {
+    "revision": "b163bd7c863ed9c4c20c",
+    "url": "/Recipes_App/static/js/main.af6e3f42.chunk.js"
+  },
+  {
+    "revision": "a6646d9f8b01beef688c",
+    "url": "/Recipes_App/static/js/2.e702ba2f.chunk.js"
+  },
+  {
+    "revision": "b163bd7c863ed9c4c20c",
+    "url": "/Recipes_App/static/css/main.e62c480b.chunk.css"
+  },
+  {
+    "revision": "b85cffdef3922ac1e354f6412749df11",
+    "url": "/Recipes_App/index.html"
+  }
+];
